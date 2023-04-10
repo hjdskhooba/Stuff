68e9389b7c8f8be9edf822b9d319a73ce1a23cb6
bakisultanov@gmail.com <bakisultanov@gmail.com>
2023-04-10 23:16:13 +0300

    add[product.js]: show item product

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,20 @@
 let trendingItems = document.querySelector(".trending__items");
 let underHundred = document.querySelector(".trending__items.underHundred");
+let id = [1];
 
 const appendTrendingItems = () => {
-    fetch("http://localhost:3000/products")
+  fetch("http://localhost:3000/products")
     .then((res) => res.json())
     .then((res) => {
-        trendingItems.innerHTML = "";
-        res.forEach((card) => {
+      trendingItems.innerHTML = "";
+      res.forEach((card) => {
         trendingItems.innerHTML += `
         <div class="card">
-        <a href="./src/pages/product.html">    
-        <div class='card-img_block'>
+        <div class='card-img_block' data-id="${card.id}">
         <img class="card-img"
         src="${card.image[0]}"
         height="110px" alt="">
-        </a>
+        
         </div>
                 <div class="card_info">
                 <div class="card_info-top">
@@ -25,21 +25,30 @@ const appendTrendingItems = () => {
                     <div class="df">
                         <span class="price">${card.price}$</span>
                         <small class="old-price">${Math.floor(
-                        (card.price / 100) * 121
+                          (card.price / 100) * 121
                         )}$</small>
                         </div>
                         <div class='df'>
                         <small class="people">${
-                            card.rating.rate + " &#11088;"
+                          card.rating.rate + " &#11088;"
                         }</small>
                         <small class="people">${
-                            card.rating.count + " votes"
+                          card.rating.count + " votes"
                         }</small>
                         </div>
                 </div>
             </div>
         </div>
         `;
+        let cardHTML = document.querySelectorAll(".card-img_block");
+        Array.from(cardHTML).forEach((el) => {
+          el.addEventListener("mouseup", async () => {
+            id.unshift(Number(el.dataset.id))
+            id.pop()
+            appendTrendingItems()
+            window.location.href = await `./src/pages/product.html?id=${id}`;
+        });
+        });
       });
     });
 };
@@ -47,7 +56,7 @@ const getUnderHundredItems = () => {
   fetch("http://localhost:3000/products?price_lte=100")
     .then((res) => res.json())
     .then((res) => {
-        underHundred.innerHTML = "";
+      underHundred.innerHTML = "";
       res.forEach((card) => {
         underHundred.innerHTML += `
             <div class="card">
@@ -65,15 +74,15 @@ const getUnderHundredItems = () => {
                         <div class="df">
                             <span class="price">${card.price}$</span>
                             <small class="old-price">${Math.floor(
-                            (card.price / 100) * 121
+                              (card.price / 100) * 121
                             )}$</small>
                         </div>
                         <div class='df'>
                         <small class="people">${
-                        card.rating.rate + " &#11088;"
+                          card.rating.rate + " &#11088;"
                         }</small>
                         <small class="people">${
-                        card.rating.count + " votes"
+                          card.rating.count + " votes"
                         }</small>
                         </div>
                     </div>
---
+++ src/js/product.js
@@ -0,0 +1,70 @@
+let row = document.querySelector(".product__body.df");
+let id = new URL(document.location).searchParams.get("id");
+const fac = new FastAverageColor();
+let tumbler = false;
+let hex = "#000000"
+
+const getProductCard = () => {
+  fetch(`http://localhost:3000/products?id=${id}`)
+    .then((res) => res.json())
+    .then((res) => {
+      res.forEach((card) => {
+        row.innerHTML = `
+                <div class="product__img df">
+                    <div class="product__img-back"><img src="${card.image[0]}" class="product__img-img"
+                            alt="" height="380px"></div>
+                    <div class="product__img__row">
+                        <div class="img-back"><img src="${card.image[0]}" class="product__img-littleOne"
+                                alt="" height="91.26px"></div>
+                        <div class="img-back"><img src="${card.image[1]}"
+                                alt="" height="91.26px"></div>
+                        <div class="img-back"><img src="${card.image[2]}" class="product__img-littleOne"
+                                alt="" height="91.26px"></div>
+                        <div class="img-back"><img src="${card.image[3]}" class="product__img-littleOne"
+                                alt="" height="91.26px"></div>
+                    </div>
+                </div>
+                <div class="product__info">
+                    <div class="product__info_info">
+                        <h3 class="product__info_info-title">${card.title}</h3>
+                        <h3 class="product__info_info-price">${card.price}$</h3>
+                        <div class="product_color">
+                            <p class="gray">color:</p>
+                            <p class="bright">rgb(${hex})</p>
+                            <span class='color' style="background: rgb(${hex})"></span>
+                        </div>
+                        <div class="product_sizes">
+                            <p class="gray">rating</p>
+                            <div class="bright">
+                                <span class="size">${card.rating.rate + " "}<s class='yellow'>&starf;</s></span>
+                                <span class="size">${card.rating.count} votes</span>
+                            </div>
+                        </div>
+                        <p class="product_description">
+                            ${card.description}
+                        </p>
+                        <button class="add-to-cart btn">Add to cart</button>
+                        <button class="add-to-favorites btn">Add to favorites</button>
+                    </div>
+                    <div class="product__info_links">
+                        <p class="product__links link1">${card.rating.count + 218} people purchased</p>
+                        <p class="product__links link2">Find in a store</p>
+                    </div>
+                </div>
+            `;
+            fac.getColorAsync(card.image[0])
+            .then(color => {
+                hex = color.rgb
+                let nums = (parseInt(hex.replace(/[^\d]/g, '')) + 30)
+                hex = nums.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
+                if(!tumbler){
+                    getProductCard()
+                    tumbler = true
+                }
+            })
+      });
+      
+    });
+};
+
+getProductCard()
